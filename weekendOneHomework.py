58c15b6fd9fb08fb90ff23654ce41b70a8912219
AHowardC <achsvgusaeng@yahoo.com>
2017-09-11 17:17:34 -0400

    notewkhwk

--- weekendOneHomework.py
+++ weekendOneHomework.py
@@ -4,12 +4,13 @@ killerGoblin = {
 	"x": 130,
 	"y": 130,
 	"speed": 15
+	# I need to find a way to subtract health from hero when killerGoblin and hero collision == True 
 }
 #Add random movement for the goblin
 
 #Change the background/theme
 Changed to waterbackground.png 
 #Add a coin or powerup that makes the hero faster
-
+still need the coin to make the hero faster 
 #Do other cool stuff
 
